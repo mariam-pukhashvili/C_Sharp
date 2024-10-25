f0c3be679d85286e710bc9f2275d99f4a49b1cf8
mpukhashvili <mariam.puxashvili@gmail.com>
2024-10-25 16:34:34 +0400

    tas2commit

---
+++ ConsoleApp1/Console.BackgroundColor = ConsoleColor.cs
@@ -0,0 +1,8 @@
+Console.BackgroundColor = ConsoleColor.Magenta;
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("-");
+
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Write("-");
